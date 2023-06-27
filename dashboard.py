c3a6baaee584c9654689f2d8f99add6026c9ceed
tristanmsct <69300092+tristanmsct@users.noreply.github.com>
2023-06-27 12:40:15 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -37,7 +37,8 @@ with tab1:
     if predict_btn:
         data = idClient
         pred = request_prediction(idClient)
-        pred = res['resultat']   
+        print(pred)
+        #pred = res['resultat']   
     
     col1, col2, col3 = st.columns([2, 1, 6])
 
